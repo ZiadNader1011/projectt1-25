496e1908927ede25198e8f8eed1b60c301dd5c7f
Said Nassar <saidnassar18@gmail.com>
2024-12-26 12:40:02 +0200

    upload

---
+++ lib/screens/create_account_patient.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../widgets/app_button.dart';
+import '../widgets/app_icon.dart';
+
+class CreateAccountPatient extends StatelessWidget {
+  const CreateAccountPatient({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+            icon: const Icon(Icons.exit_to_app),
+            onPressed: () {
+              Navigator.pop(context);
+            }),
+      ),
+      body: Column(
+        // mainAxisAlignment: .center,
+        children: [
+          AppIcon(
+            width: 200.w,
+          ),
+          Row(
+            children: [
+              SizedBox(
+                height: 26.h,
+              ),
+            ],
+          ),
+          Text(
+            'Sign up',
+            style: TextStyle(fontSize: 32.sp, fontWeight: FontWeight.w400),
+          ),
+          SizedBox(
+            height: 26.h,
+          ),
+          AppButton(
+            width: 360.w,
+            child: Center(
+              child: Text(
+                'Sign Up as parent',
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 46.h,
+          ),
+          AppButton(
+            width: 360.w,
+            child: Center(
+              child: Text(
+                'Sign up as caregiver',
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
